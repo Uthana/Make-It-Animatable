925e5bdeb3e9eeefbb49c65824d52b9bb2d2f45c
Matt Billenstein <matt@vazor.com>
2025-10-29 21:47:23 +0000

    Some comments re auth, etc

--- install.sh
+++ install.sh
@@ -5,12 +5,17 @@ set -eo pipefail
 # to clean
 # git clean -xdf
 
+# To auth with huggingface - use your username and an access-token you've
+# created
+
 if [ ! -e .venv ]; then
   /opt/uve/bin/python3 -m venv .venv
   .venv/bin/pip install -r requirements.txt
 fi
 
 if [ ! -e data/Mixamo ]; then
+  # You must share your info to use this dataset, simply visit
+  # https://huggingface.co/datasets/jasongzy/Mixamo and click "allow"
   GIT_LFS_SKIP_SMUDGE=1 git -C data clone https://huggingface.co/datasets/jasongzy/Mixamo
   git -C data/Mixamo submodule update --init
   git -C data/Mixamo lfs pull -I 'bones*.fbx,animation'
